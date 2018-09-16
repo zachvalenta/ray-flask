2faf9f598b8d76bcf613f912331cf3cc0e5392e0
zachvalenta <zjv5002@gmail.com>
2018-09-16 19:47:51 -0400

    basic GET

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask import jsonify
+
+# jsonify also handles headers
 
 app = Flask(__name__)
+
+books = [
+	{
+		'name': 'Origins of Political Order',
+		'price': 10.00,
+		'isbn': '0374533229',
+	},
+	{
+		'name': 'Political Order and Political Decay',
+		'price': 10.00,
+		'isbn': '0374535620',
+	}
+]
 
 
 @app.route('/')
@@ -8,4 +24,15 @@ def hello_world():
 	return 'hey zv'
 
 
-app.run(port=5000)
+@app.route('/books')
+def get_books():
+	return jsonify({'books': books})
+
+
+@app.route('/books/<string:isbn>')
+def get_book(isbn):
+	for book in books:
+		if book['isbn'] == isbn:
+			return jsonify({'book': book})
+		else:
+			return 'could not find book'
