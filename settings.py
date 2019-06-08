ec07254304e464233b820764590cec26ca7a4174
zachvalenta <zjv5002@gmail.com>
2019-06-08 16:08:47 -0400

    config db connection

--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, 'local.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
